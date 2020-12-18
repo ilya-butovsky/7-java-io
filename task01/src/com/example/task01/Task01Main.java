ec5b88904c263a4351c5a1783dcbe208a0ad3366
Ilya B <ilya@ml1000.net>
2020-12-18 12:56:04 +0500

    task01 [butovsky]

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class Task01Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
@@ -15,15 +15,7 @@ public class Task01Main {
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        if (inputStream == null)
-            throw new IllegalArgumentException();
-        int checkSum = 0;
-        while (true){
-            int byteFromStream = inputStream.read();
-            if (byteFromStream == -1)
-                break;
-            checkSum = Integer.rotateLeft(checkSum, 1) ^ byteFromStream;
-        }
-        return checkSum;
+        // your implementation here
+        return 0;
     }
 }
