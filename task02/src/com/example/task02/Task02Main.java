642779073afbedc83778f4feeb0e3a4ea6463d79
Ilya B <ilya@ml1000.net>
2020-12-18 12:57:18 +0500

    task02 [butovsky]

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -3,11 +3,17 @@ package com.example.task02;
 import java.io.IOException;
 
 public class Task02Main {
+
     public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+        while (System.in.available() > 0) {
+            int byteFromIn = System.in.read();
+            System.in.mark(1);
+            int nextByteFromIn = System.in.read();
+            if (!(byteFromIn == '\r' && nextByteFromIn == '\n')) {
+                System.out.write(byteFromIn);
+            }
+            System.in.reset();
+        }
+        System.out.flush();
     }
 }
